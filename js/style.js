ee65f598a26c498065070afa9c8972477da7a5b4
Deveshmonitor <deveshsen512@gmail.com>
2022-04-17 08:15:03 -0700

    dev

---
+++ js/style.js
@@ -0,0 +1,15 @@
+alert("devesh sen is owner of this website");
+console.log("hello world");
+
+
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
+
+
+//             var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+//   container: 'body'
+// })
+
+        
